db6f8f454dee2354c6bdf18365b39f31aa567f72
Erik Martinez <emrbeltane@gmail.com>
2018-06-18 14:08:21 +0200

    Update Brisca.js

--- Brisca.js
+++ Brisca.js
@@ -7,6 +7,8 @@ class Card {
     }
 }
 
+const constants = {user: "User", comp: "Computer"};
+
 // Ordered according to their score
 const names = ["Ace", "Three", "King", "Knight", "Ten", "Seven", "Six", "Five", "Four", "Two"];
 const suits = ["Clubs", "Spades", "Hearts", "Diamonds"];
@@ -68,18 +70,18 @@ function score(cards) {
 function compare(card1, card2, mainSuit, first) {
 
     // Checks who played first
-    if (first == "User") {
-        var second = "Computer";
+    if (first === constants.user) {
+        var second = constants.comp;
     } else {
-        var second = "User";
+        var second = constants.user;
     }
 
     // Evaluates who won the round
-    if (card1.suit == mainSuit && card2.suit != mainSuit) {
+    if (card1.suit === mainSuit && card2.suit !== mainSuit) {
         return first;
-    } else if (card1.suit != mainSuit && card2.suit == mainSuit) {
+    } else if (card1.suit !== mainSuit && card2.suit === mainSuit) {
         return second;
-    } else if (card1.suit != card2.suit) {
+    } else if (card1.suit !== card2.suit) {
         return first;
     } else {
         var score1 = score(card1);
@@ -92,10 +94,14 @@ function compare(card1, card2, mainSuit, first) {
     }
 }
 
+function showInfo(card) {
+    return (card.name + " of " + card.suit + "\n");
+}
+
 // TODO: Implement better AI
 function comp(card, compHand, mainSuit) {
     var compCard = compHand.shift();
-    console.log("Computer plays: " + compCard.name + " of " + compCard.suit + "\n");
+    console.log("Computer plays: " + showInfo(compCard));
     return compCard;
 }
 
@@ -111,13 +117,13 @@ function draw(n, deck) {
 function userTurn(userHand) {
     switch(userHand.length) {
         case 3:
-            var text = "Choose one card to play. You have the following cards: \n" + "1. " + userHand[0].name + " of " + userHand[0].suit + "\n2. " + userHand[1].name + " of " + userHand[1].suit + "\n3. " + userHand[2].name + " of " + userHand[2].suit + "\n";
+            var text = "Choose one card to play. You have the following cards: \n" + "1. " + showInfo(userHand[0]) + "2. " + showInfo(userHand[1]) + "3. " + showInfo(userHand[2]);
             break;
         case 2:
-            var text = "Choose one card to play. You have the following cards: \n" + "1. " + userHand[0].name + " of " + userHand[0].suit + "\n2. " + userHand[1].name + " of " + userHand[1].suit + "\n";
+            var text = "Choose one card to play. You have the following cards: \n" + "1. " + showInfo(userHand[0]) + "2. " + showInfo(userHand[1]);
             break;
         case 1:
-            var text = "Choose one card to play. You have the following cards: \n" + "1. " + userHand[0].name + " of " + userHand[0].suit + "\n";
+            var text = "Choose one card to play. You have the following card: \n" + "1. " + showInfo(userHand[0]);
             break;
     }
 
@@ -128,7 +134,7 @@ function userTurn(userHand) {
         console.log("Invalid argument, please enter a number between 1 and your handsize");
         return userTurn(userHand);
     } else {
-        console.log("You have played: " + userHand[cardChoice - 1].name + " of " + userHand[cardChoice - 1].suit);
+        console.log("You have played: " + showInfo(userHand[cardChoice - 1]);
         return userHand.splice(cardChoice - 1, 1)[0];
     }
 }
@@ -136,7 +142,7 @@ function userTurn(userHand) {
 function turn(p) {
 
     // Checks who played first
-    if (p.player == "Computer") {
+    if (p.player === constants.comp) {
         var compCard = comp(null, p.compHand, p.mainSuit);
         var userCard = userTurn(p.userHand);
         var winner = compare(compCard, userCard, p.mainSuit, p.player);
@@ -148,15 +154,15 @@ function turn(p) {
 
     // Calculates the score of the round
     var roundScore = score([userCard, compCard]);
-    if (winner == "User") {
+    if (winner === constants.user) {
         p.userScore += roundScore;
     } else {
         p.compScore += roundScore;
     }
 
     // Draw step, winner draws first
-    if (p.deck.length != 1 && p.deck.length != 0) {
-        if (winner == "User") {
+    if (p.deck.length !== 1 && p.deck.length !== 0) {
+        if (winner === constants.user) {
             p.userHand.push(draw(1, p.deck)[0]);
             p.compHand.push(draw(1, p.deck)[0]);
         } else {
@@ -166,8 +172,8 @@ function turn(p) {
     } 
     
     // Last card drawn is the face-up card from the beggining
-    else if (p.deck.length == 1) {
-        if (winner == "User") {
+    else if (p.deck.length === 1) {
+        if (winner === constants.user) {
             p.userHand.push(draw(1, p.deck)[0]);
             p.compHand.push(p.firstCard);
         } else {
@@ -195,12 +201,12 @@ function initGame() {
 
     // Coin toss to choose the first player
     if (Math.random() >= 0.5) {
-        var player = "Computer";
+        var player = constants.comp;
     } else {
-        var player = "User";
+        var player = constants.user;
     }
 
-    console.log("The first card is: " + firstCard.name + " of " + firstCard.suit + "\n");
+    console.log("The first card is: " + showInfo(firstCard));
     return {firstCard, mainSuit, userHand, compHand, player, deck, userScore, compScore};
 }
 
@@ -209,7 +215,7 @@ function newGame() {
     var parameters = initGame();
     parameters.player = turn(parameters);
 
-    while (parameters.userHand.length != 0 && parameters.compHand.length != 0) {
+    while (parameters.userHand.length !== 0 && parameters.compHand.length !== 0) {
         parameters.player = turn(parameters);
     }
 
